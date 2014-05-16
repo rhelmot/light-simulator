6ccc8947c63c3f00427d8df0e78f3c2d52c17b4e
Andrew Dutcher <andrew@andrewdutcher.com>
2014-05-15 17:00:31 -0700

    added smaller images

--- js/basicboard.js
+++ js/basicboard.js
@@ -10,7 +10,7 @@ function draw_board() {
     cx.canvas.height = 700;
     var img = $('#boardoff')[0];
     if (!img.complete) {
-        setTimeout(draw_board, 1000);
+        setTimeout(draw_board, 100);
         return;
     }
     cx.drawImage(img, 0, 0, 
@@ -18,10 +18,10 @@ function draw_board() {
         0, 0, cx.canvas.width, cx.canvas.height);
 }
 
-var BIG_X = 300,
-    BIG_Y = 950,
-    BIG_WIDTH = 300,
-    BIG_HEIGHT = 108;
+var SMALL_X = 300 / 2.65,
+    SMALL_Y = 950 / 2.65,
+    SMALL_WIDTH = 300 / 2.65,
+    SMALL_HEIGHT = 108 / 2.65;
 
 function draw_led(pin, opacity) {
     if (opacity != 0) draw_led(pin, 0);
@@ -29,11 +29,11 @@ function draw_led(pin, opacity) {
     var pini = pin-2;
     var xi = pini > 6 ? 0 : 1;
     var yi = 6 - (pini % 7);
-    var sx = BIG_X + BIG_WIDTH * xi;
-    var sy = BIG_Y + BIG_HEIGHT * yi;
+    var sx = SMALL_X + SMALL_WIDTH * xi;
+    var sy = SMALL_Y + SMALL_HEIGHT * yi;
     var ratio = img.width / cx.canvas.width;
     cx.globalAlpha = opacity == 0 ? 1 : opacity;
-    cx.drawImage(img, sx, sy, BIG_WIDTH, BIG_HEIGHT,
-        sx / ratio, sy / ratio, BIG_WIDTH / ratio, BIG_HEIGHT / ratio);
+    cx.drawImage(img, sx * ratio, sy * ratio, SMALL_WIDTH * ratio, SMALL_HEIGHT * ratio,
+        sx, sy, SMALL_WIDTH, SMALL_HEIGHT);
     cx.globalAlpha = 1;
 }
